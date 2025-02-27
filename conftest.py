e13282dd2b6b8246c6dceb866f41806acfc38b75
Mihail Hadzhiev <https://github.com/mishondrakis/ngx-cypress-test>
2025-02-27 19:37:25 +0200

    small fix of conftest file

--- conftest.py
+++ conftest.py
@@ -4,7 +4,7 @@ from playwright.async_api import async_playwright
 @pytest.fixture(scope="function", autouse=True)
 async def setup_page():
     async with async_playwright() as p:
-        browser = await p.chromium.launch(headless=False, slow_mo=500)
+        browser = await p.chromium.launch()
         new_context = await browser.new_context()
         page = await new_context.new_page()
         await page.goto("https://www.investor.bg/")
